48ee5683bd76deabbc3a99608e703449cb6d6c06
oliverkiranbrown <135071860+oliverkiranbrown@users.noreply.github.com>
2025-04-16 21:57:21 +0000

    Deploying to gh-pages from @ oliverkiranbrown/oliverkiranbrown.github.io@b2330e954ff4fcbd475cea6e36ec9166f138f421 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6F%6C%69%76%65%72%6B%69%72%61%6E%62%72%6F%77%6E@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
