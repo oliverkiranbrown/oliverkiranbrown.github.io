daaa20bda5c132aa645c510c2e12048703f23493
Oli Kiran Brown <oliverkiranbrown@gmail.com>
2025-04-26 19:39:14 +0100

    feat: updated website to fix broken links

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
 	title: "Oliver Kiran Brown",
-	url: "https://example.com/",
+	url: "https://oliverkiranbrown.github.io.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
 		name: "Oliver Kiran Brown",
 		email: "oliverkiranbrown@gmail.com",
-		url: "https://example.com/about-me/"
+		url: "https://oliverkiranbrown.github.io/About/"
 	}
 }
